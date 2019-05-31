85f1f4659ae34839543a25f497b6062aac0a2823
naoya <t_naoya_k@yahoo.co.jp>
2019-05-31 17:00:03 +0900

    Update application.scss
    誤字修正
    ルーティング、アクション名の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  def index
+  def login
   end
-  def show
+  def signup
   end
   def step1
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
   get "users/login" => "users#login"
-  get "users/signin" => "users#signin"
+  get "users/signup" => "users#signup"
   get "users/step1" => "users#step1"
   get "users/step2" => "users#step2"
   get "users/step3" => "users#step3"
