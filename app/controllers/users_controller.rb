99b120a26464d9366e7101304009f2c05fe57088
naoya <t_naoya_k@yahoo.co.jp>
2019-05-31 12:11:41 +0900

    User-signup_markup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+  end
+  def show
+  end
+  def step1
+  end
+  def step2
+  end
+  def step3
+  end
+  def step4
+  end
+  def step5
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "users/login" => "users#login"
+  get "users/signin" => "users#signin"
+  get "users/step1" => "users#step1"
+  get "users/step2" => "users#step2"
+  get "users/step3" => "users#step3"
+  get "users/step4" => "users#step4"
+  get "users/step5" => "users#step5"
 end
