98215ebea32a63e83e105c609a9202509721e1c4
naoya <t_naoya_k@yahoo.co.jp>
2019-05-31 18:26:36 +0900

    Change routes.rb
    usersのルーティングをresourcesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
-  get "users/login" => "users#login"
-  get "users/signup" => "users#signup"
-  get "users/user_registration" => "users#user_registration"
-  get "users/sms_confirmation" => "users#sms_confirmation"
-  get "users/address_registration" => "users#address_registration"
-  get "users/card_registration" => "users#card_registration"
-  get "users/registration_complete" => "users#registration_complete"
   resources :products, only: [:index, :new] do
     collection do
       get 'search'
@@ -18,6 +11,18 @@ Rails.application.routes.draw do
   end
 
   resources :cards, only: [:index] do
+  end
+
+  resources :users do
+    collection do
+      get 'login'
+      get 'signup'
+      get 'user_registration'
+      get 'sms_confirmation'
+      get 'address_registration'
+      get 'card_registration'
+      get 'registration_complete'
+    end
   end
 
 end
