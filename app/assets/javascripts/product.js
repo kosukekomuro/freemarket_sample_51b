a1cba137363aa3843b5af7ea741a4265797b30da
kosukekomuro <mmc.komuro@gmail.com>
2019-06-01 13:45:29 +0900

    add function image slide

---
+++ app/assets/javascripts/product.js
@@ -0,0 +1,10 @@
+$(function() {
+$('.main-slick-image').slick({
+    prevArrow:'<i class="fa fa-angle-left slide-arrow prev-arrow"></i>',
+    nextArrow:'<i class="fa fa-angle-right slide-arrow next-arrow"></i>',
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 800
+  });
+});
