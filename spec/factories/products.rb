2b899721598063716cef4909b5ae1722d14cd68f
shota mochizuki <asahina@shota-no-MacBook-Pro.local>
2019-06-04 13:18:48 +0900

    Modify display product list test

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,16 +3,15 @@ FactoryBot.define do
   factory :product do
     name                  {"セーター"}
     price                 {"1500"}
-    trading_evaluation_id {"1"}
+    trading_evaluation {create(:trading_evaluation)}
     brand_id              {"1"}
     category_id           {"1"}
-    seller_id             {"1"}
-    buyer_id              {"1"}
-    size_id               {"1"}
+    seller                {create(:user)}
+    size                  {create(:size)}
     condition_id          {"1"}
     delivery_method_id    {"1"}
-    prefecture_id         {"1"}
-    delivery_day_id       {"1"}
+    prefecture            {create(:prefecture)}
+    delivery_day          {create(:delivery_day)}
     description           {"セーターです。"}
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
+require 'faker'
+
 FactoryBot.define do
 
   factory :user do
-    email                  {"aa@aa.com"}
-    password               {"00000000"}
-    password_confirmation  {"00000000"}
+    password = Faker::Internet.password(8)
+    email {Faker::Internet.free_email}
+    password {password}
+    password_confirmation {password}
   end
 
 end
