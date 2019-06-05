395b52f8442a95297d6c8f165f6230fcaa31ffac
naoya <t_naoya_k@yahoo.co.jp>
2019-06-05 18:26:24 +0900

    Update_migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_071940) do
+ActiveRecord::Schema.define(version: 2019_06_04_045115) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "brand", null: false
@@ -144,13 +144,11 @@ ActiveRecord::Schema.define(version: 2019_06_05_071940) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "card_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "category_sizes", "categories"
   add_foreign_key "category_sizes", "sizes"
+  add_foreign_key "images", "products"
   add_foreign_key "user_addresses", "users"
   add_foreign_key "user_details", "users"
-  add_foreign_key "images", "products"
 end
