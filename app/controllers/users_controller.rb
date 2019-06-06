ac7b4bdf40963c2181db102314ffce5621e2f49e
Takaya Saito <s.takaya.jumbo8@gmail.com>
2019-06-06 13:06:08 +0900

    空行を挟む

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,33 @@
 class UsersController < ApplicationController
+
   def login
   end
+
   def logout
   end
+
   def signup
   end
+
   def user_registration
   end
+
   def sms_confirmation
   end
+
   def address_registration
   end
+
   def card_registration
   end
+
   def registration_complete
   end
+
   def show
   end
+
   def myconfirmation
   end
+
 end
