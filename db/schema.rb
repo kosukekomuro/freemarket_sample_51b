146c03fecaab467a52d3e6ea16c4d0fb2836eafb
kosukekomuro <mmc.komuro@gmail.com>
2019-06-19 17:16:40 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -47,12 +47,6 @@ ActiveRecord::Schema.define(version: 2019_06_16_095347) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "delivery_fee_burdens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "burden", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "delivery_methods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
